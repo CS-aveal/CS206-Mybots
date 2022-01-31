522629aa574558e12bdc0e8b34ad78a246c1b2cf
Austin Veal <aveal@uvm.edu>
2022-01-31 16:29:35 -0500

    got first connection working

--- generate.py
+++ generate.py
@@ -1,25 +1,21 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
-i = 0
-j = 0
-l = 0
-x = 1
-y = 1
-z = 1
 
-while j < 5:
-  while l < 5:
-    while i < 10:
-      pyrosim.Send_Cube(name="Box2", pos=[j,l,i + 0.5] , size=[x,y,z])
-      x = x * .9
-      y = y * .9
-      z = z * .9
-      i = i + 1
-    i = 0
-    x = 1
-    y = 1
-    z = 1
-    l = l + 1
-  l = 0
-  j = j + 1
-pyrosim.End()
+def create_World():
+  pyrosim.Start_SDF("world.sdf")
+  pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+  pyrosim.End()
+
+def create_Robot():
+  pyrosim.Start_URDF("body.urdf")
+  pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
+  pyrosim.Send_Joint( name = "Torso_Leg" , parent= "Torso" , child = "Leg" ,type = "revolute", position = [0,0,1])
+  pyrosim.Send_Cube(name="Leg", pos=[0,0,0.5] , size=[1,1,1])
+  pyrosim.End()
+
+
+
+def main():
+  create_Robot()
+  create_World()
+
+main()
