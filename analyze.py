f3e11b6f62e1d999ee375a866ec3d431c84ebb87
Austin Veal <aveal@uvm.edu>
2022-02-14 10:41:27 -0500

    added analyze.py and simulate.py

--- analyze.py
+++ analyze.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy
+
+backLegSensorValues = numpy.load('./data/backLegSensorValues.npy')
+frontLegSensorValues = numpy.load('./data/frontLegSensorValues.npy')
+
+plt.plot(backLegSensorValues, label='Back Leg Sensor', linewidth=5)
+plt.plot(frontLegSensorValues, label='Front Leg Sensor')
+plt.legend()
+plt.show()
+
