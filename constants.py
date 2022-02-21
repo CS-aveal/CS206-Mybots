144626ae35c986d4d3bea8eacb65c8b02bb9150d
Austin Veal <aveal@uvm.edu>
2022-02-21 14:48:52 -0500

    added a constants file to easily manipulate variables but not working properly yet

---
+++ constants.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#front leg sensor values
+amplitudeFL = np.pi/4
+frequencyFL = 20
+phaseOffsetFL = np.pi/4
+
+#backleg sensor values
+amplitudeBL = np.pi/4
+frequencyBL = 20
+phaseOffsetBL = 0
