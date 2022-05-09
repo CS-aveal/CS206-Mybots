367a3d3f80f1c182b72200e8f7c58622b62d0826
Austin Veal <aveal@uvm.edu>
2022-05-09 17:13:02 -0400

    changed constants.py

--- constants.py
+++ constants.py
@@ -19,7 +19,7 @@ loop = 1000
 y = np.linspace(0,2 * np.pi, num=1000)
 targetAngles = np.sin(y)
 
-numberOfGenerations = 2
+numberOfGenerations = 10
 
 defaultFit = -0.018336037653125914
 
@@ -27,7 +27,7 @@ numSensorNeurons = 9
 
 numMotorNeurons = 9
 
-populationSize = 2
+populationSize = 10
 
 motorJointRange = 0.2
 
